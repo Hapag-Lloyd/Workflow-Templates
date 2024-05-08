38f8dfd91da56a68e665f07bb5e50f2e7d13ab79
Matthias Kay <matthias.kay@hlag.com>
2024-05-08 17:17:00 +0200

    feat: init (#2)
    # Description
    
    Initial upload of all workflow files.
    
    # Verification
    
    Manually in test repository.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Workflow-Templates
-Template files for workflows
+
+This repository stores templates used to set up workflows for new repositories.
+
+## Set up a new repository
+
+Clone this repository to your disk and run `setup-workflows.sh`. It copies the necessary files to the correct
+locations in your new repository.
+
+```bash
+./setup-workflows.sh <path-to-new-repository> <type>
+```
+
+Search for `TODO` in the copied files and replace the placeholders with the correct values. The same script can be used to update
+all files in case of major changes in the templates.
+
+:warning: Make sure to run the script from the latest `main` branch to get the most recent templates.
+
+## What you get
+
+### Default setup
+
+- Release management is done with [semantic-release](https://github.com/semantic-release/semantic-release). Releases are automatically tagged and published on GitHub. Special
+  releases for Maven Central and Terraform modules are supported.
+- stale issue and PR management
+- welcome message for contributors
+- linters for all files
+- PRs are checked for semantic commit titles to ensure an automatic release
+- ChatOps to run workflows from comments
+
+## For Developers - Repository Layout
+
+1. Add all workflows to `.github/workflows/`, otherwise they can't be referenced from the repositories.
+2. Workflows with `this_` prefix are used for this repository only.
+3. Workflows with `default_` prefix are added to every new repository. Otherwise use the correct prefix for the project type.
+
+The script to set up the workflows for new repositories is `setup-workflows.sh`. It copies the necessary files to the new
+repository. It starts with the default workflows and adds the specific ones based on the project type. In case of a filename clash,
+the specific template overwrites the default one (exception: `.gitignore` These files are concatenated).
+
+Use 
+
+```bash
+# USE_REPOSITORY
+#  push:
+#    branches:
+#      - main
+# /USE_REPOSITORY
+```
+
+in the file to describe the triggers which should be used in the repository. The script will automatically replace the triggers
+marked with `USE_WORKFLOW` which are valid within this repository only.
---
+++ setup-workflows.sh
@@ -0,0 +1,179 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+destination_path=""
+repository_type=""
+
+function check_prerequisites() {
+  if ! command -v yq &> /dev/null; then
+    echo "yq is not installed. https://github.com/mikefarah/yq"
+    exit 1
+  fi
+}
+
+function check_and_set_parameters() {
+  POSITIONAL_ARGS=()
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --*|-*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+      *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+  done
+
+  set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+  # check for 3 mandatory positional arguments
+  if [ "${#POSITIONAL_ARGS[@]}" -ne 2 ]; then
+    echo "Usage: $0 <path-to-new-repo> <repository-type>"
+    exit 1
+  fi
+
+  destination_path=$1
+  repository_type=$2
+
+  # check if the directory exists
+  if [ ! -d "$destination_path" ]; then
+    echo "The repository $destination_path does not exist."
+    exit 2
+  fi
+
+  # check for correct type: docker, github-only, maven, terraform_module
+  if [ "$repository_type" != "github-only" ] && [ "$repository_type" != "maven" ] && [ "$repository_type" != "terraform_module" ] && [ "$repository_type" != "docker" ]; then
+    echo "The repository type $repository_type is not supported."
+    exit 3
+  fi
+}
+
+check_prerequisites
+check_and_set_parameters "$@"
+
+# enable nullglob to prevent errors when no files are found
+shopt -s nullglob
+
+# basic setup for all types
+mkdir -p "$destination_path/.github/workflows"
+cp .github/workflows/default_* .github/workflows/release_* "$destination_path/.github/workflows/"
+
+# we do not have special files for simple GitHub projects, this is handled by the default setup
+if [ "$repository_type" != "github-only" ]; then
+  cp ".github/workflows/${repository_type}"_* "$destination_path/.github/workflows/"
+fi
+
+#
+# Fix the "on" clause in the workflow files, remove all jobs and set a reference to this repository
+#
+
+# iterate over each file in the directory
+for file in "$destination_path"/.github/workflows/*.yml
+do
+  base_name=$(basename "$file")
+
+  # remove everything else as we will reference the file in this repository
+  sed -i '/jobs:/,$d' "$file"
+
+  cat >> "$file" <<-EOF
+jobs:
+  default:
+    uses: Hapag-Lloyd/Workflow-Templates/.github/workflows/$base_name
+    secrets: inherit
+EOF
+
+  # add TODOs for the parameters of the workflow
+  # false positive, variable is quoted
+  # shellcheck disable=SC2086
+  if [ "$(yq '.on["workflow_call"] | select(.inputs) != null' $file)" == "true" ]; then
+    cp "$file" "$file.bak"
+    echo "    with:" >> "$file"
+
+    yq '.on.workflow_call.inputs | keys | .[]' "$file".bak | while read -r input; do
+      type=$(yq ".on.workflow_call.inputs.$input.type" "$file".bak)
+      required=$(yq ".on.workflow_call.inputs.$input.required" "$file".bak)
+      description=$(yq ".on.workflow_call.inputs.$input.description" "$file".bak)
+
+      cat >> "$file" <<-EOF
+      # TODO insert correct value for $input
+      # type: $type
+      # required: $required
+      # description: $description
+      $input: "my-special-value"
+EOF
+    done
+
+    rm "$file.bak"
+  fi
+
+  # remove the comment char for all lines between USE_REPOSITORY and /USE_REPOSITORY in the file
+  sed -i '/USE_REPOSITORY/,/\/USE_REPOSITORY/s/^#//' "$file"
+
+  # remove the everything between USE_WORKFLOW and /USE_WORKFLOW
+  sed -i '/USE_WORKFLOW/,/\/USE_WORKFLOW/d' "$file"
+
+  # remove the marker lines
+  sed -i '/USE_REPOSITORY/d' "$file"
+  sed -i '/\/USE_REPOSITORY/d' "$file"
+  sed -i '/USE_WORKFLOW/d' "$file"
+  sed -i '/\/USE_WORKFLOW/d' "$file"
+done
+
+#
+# Remove the prefix from the workflow files
+#
+prefixes=("default_" "terraform_module_" "docker_" "maven_")
+
+# iterate over each file in the directory
+for file in "$destination_path"/.github/workflows/*.yml
+do
+  # get the base name of the file
+  base_name=$(basename "$file")
+
+  # iterate over each prefix
+  for prefix in "${prefixes[@]}"
+  do
+    # check if the file name starts with the prefix
+    if [[ $base_name == $prefix* ]]; then
+      # remove the prefix
+      new_name=${base_name#"$prefix"}
+
+      # rename the file
+      mv "$file" "$destination_path/.github/workflows/$new_name"
+
+      # break the loop as the prefix has been found and removed
+      break
+    fi
+  done
+done
+
+#
+# Remove the suffix from the workflow files
+#
+suffixes=("_callable.yml")
+
+# iterate over each file in the directory
+for file in "$destination_path"/.github/workflows/*.yml
+do
+  # get the base name of the file
+  base_name=$(basename "$file")
+
+  # iterate over each suffix
+  for suffix in "${suffixes[@]}"
+  do
+    # check if the file name starts with the prefix
+    if [[ $base_name == *$suffix ]]; then
+      # remove the suffix
+      new_name="${base_name%"$suffix"}.yml"
+
+      # rename the file
+      mv "$file" "$destination_path/.github/workflows/$new_name"
+
+      # break the loop as the suffix has been found and removed
+      break
+    fi
+  done
+done
