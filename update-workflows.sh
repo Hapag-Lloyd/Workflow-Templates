fbb3aa9efe7ec561ceafd79163fd49fd8ed61a4e
Matthias Kay <matthias.kay@hlag.com>
2024-05-23 20:55:51 +0200

    Update update-workflows.sh

--- update-workflows.sh
+++ update-workflows.sh
@@ -52,6 +52,15 @@ function create_commit_and_pr() {
   git commit -m "update workflows to latest version"
   git push --set-upstream origin update-workflows
 
+  body=$(cat <<-EOF
+    # Description
+
+    This PR updates all workflows to the latest version.
+
+    # Verification
+
+    Done by the workflows in this feature branch, except for the release workflow.
+EOF
   gh pr create --title "ci: update workflows to latest version" --body "" --base main
   gh pr view --web
 }
