5b746b71031afd1f4b164834393b56f347eb18bb
Matthias Kay <matthias.kay@hlag.com>
2025-01-09 14:51:04 +0100

    script

--- .github/update_workflows.sh
+++ .github/update_workflows.sh
@@ -201,7 +201,7 @@ cd "$repository_path" || exit 8
 echo "Fetching the latest version of the workflows"
 
 latest_template_path=$(mktemp -d -t repository-template-XXXXX)
-gh repo clone -q https://github.com/Hapag-Lloyd/Workflow-Templates.git "$latest_template_path"
+gh repo clone https://github.com/Hapag-Lloyd/Workflow-Templates.git "$latest_template_path" -- -q
 
 # TODO
 (cd "$latest_template_path" && git checkout kayma/update-workflows)
