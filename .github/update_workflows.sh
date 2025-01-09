56881679d5a291cc520aee663d40700303eb975a
Matthias Kay <matthias.kay@hlag.com>
2025-01-09 13:09:31 +0100

    script

--- .github/update_workflows.sh
+++ .github/update_workflows.sh
@@ -261,7 +261,7 @@ do
   echo $latest_template_path
   version_reference=$(
     echo $latest_template_path
-    return
+    exit 0
     cd "$latest_template_path" || exit 9
 
     # add a reference to this repository which holds the workflow
